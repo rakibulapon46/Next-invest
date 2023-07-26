66ba8fc852507ad4d0f4106be159c5e23103fe62
rakibulapon46 <rakibulapon46@gmail.com>
2023-07-26 11:32:55 +0600

    push

---
+++ index.js
@@ -0,0 +1,9 @@
+const logIn = document.querySelector(".btn1");
+logIn.addEventListener("click", () => {
+    window.open("./login.html");
+});
+
+const reGi = document.querySelector(".btn2");
+reGi.addEventListener("click", () => {
+    window.open("./registeration.html");
+});
